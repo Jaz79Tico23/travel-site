e4cdf0ee82931afd8b153c48edf8e50cb98172e2
Tim <tmisshawk@aol.com>
2017-12-16 13:33:09 -0600

    modified watch.js file

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -13,12 +13,13 @@ gulp.task('watch', function() {
     watch('./app/index.html', function() {
         browserSync.reload();
     });
+
     watch('./app/assets/styles/**/*.css', function() {
         gulp.start('cssInject');
     });
 });
 
-gulp.task('cssInject', ['styles'] ,function() {
+gulp.task('cssInject', ['styles'], function() {
     return gulp.src('./app/temp/styles/styles.css')
     .pipe(browserSync.stream());
 });
